5ca5348317f73642bbd47868de460955f4e982dc
Alexandr Bochev <abochev@gmail.com>
2022-07-02 00:02:13 +0300

    classes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 (function () {
   const burgerItem = document.querySelector('.burger');
-  const menuCloseItem = document.querySelector('.header-nav-close');
-  const menuCloseLinks = document.querySelector('.header-list');
-  const menu = document.querySelector('.header-nav');
+  const menuCloseItem = document.querySelector('.header_nav_close');
+  const menuCloseLinks = document.querySelector('.header_list');
+  const menu = document.querySelector('.header_nav');
   const header = document.querySelector('.header');
   const warningClose = document.querySelector('.close');
 
   burgerItem.addEventListener('click', () => {
-    menu.classList.add('header-nav-active');
+    menu.classList.add('header_nav_active');
   });
   menuCloseItem.addEventListener('click', () => {
-    menu.classList.remove('header-nav-active');
+    menu.classList.remove('header_nav_active');
   });
   menuCloseLinks.addEventListener('click', () => {
-    menu.classList.remove('header-nav-active');
+    menu.classList.remove('header_nav_active');
   });
   warningClose.addEventListener('click', () => {
-    header.classList.add('header-active');
+    header.classList.add('header_active');
   });
 }());
